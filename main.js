d3969a00b717e892e34f00a330345203dd172c2c
KrissyvanD <58208561+KrissyvanD@users.noreply.github.com>
2020-07-06 00:01:58 +0200

    commit 2

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+//making a function where the value of time is calculated
 function calculate(){
-
+//first get the distance input from the user
 	var distance = document.getElementById('dis').value;
+//set an amount for the speed that the user isn't possible of changing
 	var speed = 7000;
-	var time = 0;
+//Set the time 
 	var time = ((distance/speed)*60);
 
 	document.getElementById("timeValue").value= time;
